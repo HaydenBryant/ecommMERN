f7a08ba1ca0afe23ac81e7822012df171ac0d04d
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-06-13 13:26:51 -0400

    mongo connect

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
+// const bodyParser = require("body-parser")
 
-const posts = require("./routes/api/posts.js")
+// const posts = require("./routes/api/posts.js")
 
 const app = express()
+
+const db = require("./config/keys").mongoURI;
+
+mongoose.connect(db, {useNewUrlParser: true})
+    .then(() => console.log("mongo db has connected")
+    )
+    .catch(err => console.log(err));
+
+
+// app.use("/api/posts", posts)
+
 
 const PORT = process.env.PORT || 5000;
 
