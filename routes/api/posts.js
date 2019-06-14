93d674ba232f4e2b378b516acda8450e6eb795d2
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-06-14 13:12:36 -0400

    react added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,9 +4,21 @@ const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const jsonParser = bodyParser.json();
 
+//mongoose post schema
+const post = require("../../models/Post.js")
+
+//validations
+
 
 // @route    GET /api/posts/test
 // @desc     tests get route
 // @access   public
 router.get("/testing", (req, res) => {res.send("Hey tom, it's Maddie")
+})
+
+router.post('/', jsonParser, (req, res) => {
+    const newPost = {
+        
+    }
+
 })
