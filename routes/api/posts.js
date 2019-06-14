8361a5a6d60f2a58a5b8f829ea94889ca8e0fb3b
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-06-14 13:19:56 -0400

    version 2. shortened Post method for now

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -17,8 +17,29 @@ router.get("/testing", (req, res) => {res.send("Hey tom, it's Maddie")
 })
 
 router.post('/', jsonParser, (req, res) => {
-    const newPost = {
-        
-    }
+
+    const newPost = new Post({
+        email: req.body.email,
+        date: req.body.date,
+        stick: {
+            brand: req.body.stick.brand,
+            material: req.body.stick.material,
+            model: req.body.stick.model,
+            ageGroup: req.body.stick.ageGroup,
+            hand: req.body.stick.hand,
+            length: req.body.stick.length,
+            curve: req.body.stick.curve,
+            pattern: req.body.stick.pattern,
+            color: req.body.stick.color,
+            condition: req.body.stick.condition,
+            description: req.body.stick.description,
+            price: req.body.stick.price
+        }
+    })
+
+    //save to db
+    newPost.save().then(data => res.json(data))
 
 })
+
+module.exports = router
